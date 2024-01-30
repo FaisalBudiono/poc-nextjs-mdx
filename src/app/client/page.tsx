f5ad7f37eca5f83353a40321c28feef17edfa49b
Faisal Budiono <ucul.project@gmail.com>
2024-01-30 23:51:13 +0700

    feat: add example for custom components

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -3,15 +3,22 @@
 import { MDXRemote } from 'next-mdx-remote'
 import { markdownText } from '../_storage/markdown'
 import { useMarkdownSerializerSWR } from '../_hooks/useMarkdownSerializerSWR'
+import { MDXComponents } from 'mdx/types'
+
+const components = {
+  h2: ({ children }) => <h2 className="text-red-600">{children}</h2>,
+} satisfies Readonly<MDXComponents>
 
 export default function ClientComponent() {
   const { data, isLoading } = useMarkdownSerializerSWR(markdownText)
 
   return (
-    <div>
-      <h1>HUAHAHAHAHAHA</h1>
-
-      {!isLoading && data !== undefined && <MDXRemote {...data} />}
+    <div className="m-10">
+      <article className="prose">
+        {!isLoading && data !== undefined && (
+          <MDXRemote {...data} components={components} />
+        )}
+      </article>
     </div>
   )
 }
